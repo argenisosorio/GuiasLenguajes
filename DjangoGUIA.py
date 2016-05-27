69a49284d355a5414bd7141954adb646400fbbdb
aosorio <aosorio@cenditel.gob.ve>
2016-05-27 12:14:24 -0430

    Arreglados varios detalles en declaraciones de comandos y sus descripciones

--- DjangoGUIA.py
+++ DjangoGUIA.py
@@ -426,3 +426,22 @@ Ejemplo:
 			-base.html
 		    -index.html
 		    -index2.html
+
+Ejemplos:
+
+// Declarando la clase Articulo y Comentarios para una publicacion,
+y estableciendo la relacion entre los comentarios y un articulo
+
+from django.db import models
+
+class Articulo(models.Model):
+   autor = models.CharField(max_length = 30)
+   titulo = models.CharField(max_length = 100)
+   texto = models.TextField()
+   fecha = models.DateTimeField()
+
+class Comentario(models.Model):
+   nombre = models.CharField(max_length = 200)
+   cuerpo = models.TextField()
+   fecha_pub = models.DateTimeField('fecha publicacion')
+   articulo = models.ForeignKey(Articulo)
