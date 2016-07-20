6440fa71ea3e4b4ca47dd7cc14337994f2db8586
aosorio <aosorio@cenditel.gob.ve>
2016-07-20 10:15:31 -0430

    Agregado manual para conexion ssh por explorador de archivos y otras tecnicas

--- C++andC-GUIA.cpp
+++ C++andC-GUIA.cpp
@@ -407,6 +407,63 @@ int main() {
 return 0;
 }
 
+########################################################
+##### Teoría sobre librerias estáticas y dinámicas #####
+########################################################
+
+Según vamos haciendo programas, nos damos cuenta que algunas partes del código se utilizan en muchos de ellos. Por ejemplo, podemos tener
+varios programas que utilizan números complejos y las funciones de suma, resta, etc son comunes. También es posible, por ejemplo, que nos
+guste hacer juegos, y nos damos cuenta que estamos repitiendo una y otra vez el código para mover una imagen por la pantalla.
+
+Sería estupendo poder meter esas funciones en un directorio separado de los programas concretos y tenerlas ya compiladas, de forma que podamos
+usarlas siempre que queramos. Las ventajas enormes de esto son:
+-No tener que volver a escribir el código (o hacer copy-paste).
+-Nos ahorraremos el tiempo de compilar cada vez ese código que ya está compilado. Además, ya sabemos que mientras hacemos un programa, probamos
+corregimos, hay que compilar entre muchas y muchas veces.
+-El código ya compilado estará probado y será fiable. No las primeras veces, pero sí cuando ya lo hayamos usado en 200 programas distintos y le
+hayamos ido corrigiendo los errores.
+
+La forma de hacer esto es hacer librerías. Una librería son una o más funciones que tenemos ya compiladas y preparadas para ser utilizadas en
+cualquier programa que hagamos.  Hay que tener el suficiente ojo cuando las hacemos como para no meter ninguna dependencia de algo concreto de
+nuestro programa. Por ejemplo, si hacemos nuestra función de mover la imagen, tendremos que hacer la función de forma que admita cualquier imagen
+ya que no nos pegaría nada Lara Croft dando saltos en un juego estilo space invaders.
+
+--- Cómo tenemos que organizar nuestro código ---
+
+Para poder poner nuestro código en una librería, necesitamos organizarlo de la siguiente manera:
+-Uno o más ficheros fuente .c o .cpp con el código de nuestras funciones.
+-Uno o más ficheros de cabecera, las librerías .h con los tipos (typedefs, structs y enums) y prototipos de las funciones que queramos que se puedan utilizar.
+
+--- Librerias estáticas y dinámicas ---
+
+En linux podemos hacer dos tipos de librerías: estáticas y dinámicas.
+
+Una librería estática es una librería que 'se copia' en nuestro programa cuando lo compilamos. Una vez que tenemos el ejecutable de nuestro programa,
+la librería no sirve para nada (es un decir, sirve para otros futuros proyectos). Podríamos borrarla y nuestro programa seguiría funcionando, ya que
+tiene copia de todo lo que necesita. Sólo se copia aquella parte de la librería que se necesite. Por ejemplo, si la librería tiene dos funciones y
+nuestro programa sólo llama a una, sólo se copia esa función.
+
+Una librería dinámica NO se copia en nuestro programa al compilarlo. Cuando tengamos nuestro ejecutable y lo estemos ejecutando, cada vez que el código
+necesite algo de la librería, irá a buscarlo a ésta. Si borramos la librería, nuestro programa dará un error de que no la encuentra.
+
+--- ¿Cuáles son las ventajas e inconvenientes de cada uno de estos tipos de librerías? ---
+
+-Un programa compilado con librerías estáticas es más grande, ya que se hace copia de todo lo que necesita.
+-Un programa compilado con librerías estáticas se puede llevar a otro ordenador sin necesidad de llevarse las librerías.
+-Un programa compilado con librerías estáticas es, en principio, más rápido en ejecución. Cuando llama a una función de la librería, la tiene en su
+código y no tiene que ir a leer el fichero de la librería dinámica para encontrar la función y ejecutarla.
+-Si cambiamos una librería estática, a los ejecutables no les afecta. Si cambiamos una dinámica, los ejecutables se ven afectados. Esto es una ventaja
+si hemos cambiado la librería para corregir un error (se corrige automáticamente en todos los ejecutables), pero es un inconveniente si tocar eso nos
+hace cambiar los ejecutables (por ejemplo, hemos añadido un parámetro más a una función de la librería, los ejecutables ya hechos dejan de funcionar).
+
+--- ¿Qué tipo de librería uso entonces? ---
+
+Es como siempre una cuestión de compromiso entre las ventajas y los inconvenientes. Para programas no muy grandes y por simplicidad, se suelen usar
+librerías estáticas. Las dinámicas están bien para programas enormes o para librerías del sistema, que como están en todos los ordenadores con linux
+no es necesario andar llevándoselas de un lado a otro.
+
+En unix las librerías estáticas suelen llamarse libnombre.a y las dinámicas libnombre.so, donde nombre es el nombre de nuestra librería. 
+
 ############################
 ##### Uso de Librerias #####
 ############################
@@ -445,5 +502,3 @@ int main() {
     cout<<suma<<endl;
     return 0;
 }
-
-
