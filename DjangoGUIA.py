075849f73119a10a4fef98d26231ba5cc0ccfee5
aosorio <aosorio@cenditel.gob.ve>
2016-08-02 16:58:58 -0430

    Agregados ejemplos de ajax

--- DjangoGUIA.py
+++ DjangoGUIA.py
@@ -445,3 +445,23 @@ class Comentario(models.Model):
    cuerpo = models.TextField()
    fecha_pub = models.DateTimeField('fecha publicacion')
    articulo = models.ForeignKey(Articulo)
+
+##### Pasando un diccionario en el render #####
+
+xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries", "Blueberries", "Dates", "Grapefruit", "Kiwi", "Lemon"]
+ydata = [52, 48, 160, 94, 75, 71, 490, 82, 46, 17]
+chartdata = {'x': xdata, 'y': ydata}
+charttype = "pieChart"
+chartcontainer = 'piechart_container'
+data = {
+    'charttype': charttype,
+    'chartdata': chartdata,
+    'chartcontainer': chartcontainer,
+    'extra': {
+        'x_is_date': False,
+        'x_axis_format': '',
+        'tag_script_js': True,
+        'jquery_on_ready': False,
+    }
+}
+return render_to_response('piechart.html', data)
