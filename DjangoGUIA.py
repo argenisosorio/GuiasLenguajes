a46c300cf86ead1ae8617b2bcb701a66208fe6dc
aosorio <argenisosorio580@hotmail.com>
2017-02-07 15:40:12 -0400

    Agregados ejemplos de mostrar mensajes de estado en django luego del post o algo

--- DjangoGUIA.py
+++ DjangoGUIA.py
@@ -542,6 +542,15 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+##### Ejemplo de admin.py para mostrar los modelos en el admin de django #####
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+
 ##### Pasando un diccionario en el render #####
 
 xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries", "Blueberries", "Dates", "Grapefruit", "Kiwi", "Lemon"]
@@ -712,3 +721,71 @@ INSTALLED_APPS = (
     'django_extensions',
     ...
 )
+
+#############################################
+##### Mensajes / messages al hacer post #####
+#############################################
+
+- En la views.py
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView
+from django.views import generic
+from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from .models import *
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+
+class PostLista(ListView):
+    model = Post
+
+
+
+class PostCrear(SuccessMessageMixin,CreateView):
+    model = Post
+    fields = ['autor', 'titulo', 'cuerpo', 'fecha']
+    success_url = reverse_lazy('post_lista')
+    success_message = "Se creó la publicación con éxito"
+
+
+
+class PostActualizar(SuccessMessageMixin,UpdateView):
+    model = Post
+    fields = ['autor', 'titulo', 'cuerpo', 'fecha']
+    success_url = reverse_lazy('post_lista')
+    success_message = "Se actualizó la publicación con éxito"
+
+
+
+class PostEliminar(SuccessMessageMixin,DeleteView):
+    model = Post
+    fields = ['autor', 'titulo', 'cuerpo', 'fecha']
+    success_url = reverse_lazy('post_lista')
+    success_message = "Se eliminó la publicación con éxito"
+
+- En el template:
+
+{% if messages %}
+<ul class="messages">
+{% for message in messages %}
+<li{% if message.tags %} class="{{ message.tags }}"{% endif %}>{{ message }}</li>
+{% endfor %}
+</ul>
+{% endif %}
+
+ó
+
+{% if messages %}
+  {% for message in messages %}
+    <div class="alert alert-success" role="alert">
+      {% if message.tags %}{% endif %}>
+      {{ message }}
+    </div>
+  {% endfor %}
+{% endif %}
+
