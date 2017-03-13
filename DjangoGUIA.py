9d8b17ccaaa81b1d3fec9f790fd4e643f17507ca
Argenis Osorio <argenisosorio580@hotmail.com>
2017-03-13 01:21:58 -0400

    Agregados tips a las guías de Django

--- DjangoGUIA.py
+++ DjangoGUIA.py
@@ -1103,3 +1103,34 @@ class PostCrear(SuccessMessageMixin,CreateView):
     form_class = PostForm
     success_url = reverse_lazy('post_lista')
     success_message = "Se creó la publicación con éxito"
+
+##################################################
+##### forms.py para el modelo User de django #####
+##################################################
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(label='First Name')
+    last_name = forms.CharField(label='Last Name')
+    email = forms.EmailField(label='Email Address')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+  
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+            return user
