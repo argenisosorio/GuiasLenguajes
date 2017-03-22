c94a78b707b4f69f0b25c58fb1be0dcaf22c67b5
aosorio <argenisosorio580@hotmail.com>
2017-03-22 14:59:20 -0400

    Agregado ejemplo a la guía RoR

--- RubyOnRailsGUIA.rb
+++ RubyOnRailsGUIA.rb
@@ -114,6 +114,8 @@ $ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804B
   
 # Instalación rápida de RVM #
 
+# apt-get install curl
+
 $ \curl -sSL https://get.rvm.io | bash -s stable --rails --ruby
   
 Este comando instala las últimas versiones estables de rvm, ruby y rails.
