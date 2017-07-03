738d138f4ec736a71a9208cc3b9a5c26f3b70cbc
aosorio <argenisosorio580@hotmail.com>
2017-07-03 16:57:30 -0400

    Estables ejemplos de success_messages en las clases genericas de Django

--- DjangoGUIA.py
+++ DjangoGUIA.py
@@ -1402,6 +1402,72 @@ class Borrar(DeleteView):
   {% endfor %}
 {% endif %}
 
+##### Usando los tag en el crud de django #####
+
+# view.py
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from registro.models import Persona
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+
+
+class Consultar(ListView):
+    model = Persona
+
+
+class Registrar(CreateView):
+    model = Persona
+    fields = ['nombre', 'cedula']
+    success_url = reverse_lazy('registro:consultar')
+    success_message = "Se agrego una persona con éxito"
+
+    def form_valid(self, form):
+        self.object = form.save()
+        messages.success(self.request, self.success_message)
+        return super(Registrar, self).form_valid(form)
+
+
+class Editar(UpdateView):
+    model = Persona
+    fields = ['nombre', 'cedula']
+    success_url = reverse_lazy('registro:consultar')
+    success_message = "Se actualizo una persona con éxito"
+
+    def form_valid(self, form):
+        self.object = form.save()
+        messages.warning(self.request, self.success_message)
+        return super(Editar, self).form_valid(form)
+
+
+class Borrar(DeleteView):
+    model = Persona
+    success_url = reverse_lazy('registro:consultar')
+    success_message = "Se elimino una persona con éxito"
+
+    def delete(self, request, *args, **kwargs):
+        #messages.success(self.request, self.success_message)
+        #messages.warning(self.request, self.success_message)
+        messages.info(self.request, self.success_message)
+        return super(Borrar, self).delete(self, request, *args, **kwargs)
+
+# base.html
+
+{% if messages %}
+  {% for message in messages %}
+    <div class="alert alert-{{ message.tags }} alert-dismissable">
+      <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+        <strong>
+          {% if message.tags %}{% endif %}
+          {{ message }}
+        </strong>
+    </div>
+  {% endfor %}
+{% endif %}
+
 #########################################
 ##### Uso de formularios / forms.py #####
 ########################################
