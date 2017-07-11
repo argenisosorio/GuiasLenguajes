f467eb3612f3eea187815b070bba6f24fd6f0d74
aosorio <argenisosorio580@hotmail.com>
2017-07-11 14:57:24 -0400

    Añadidas las guías aún muy básicas de Xen y Docker

--- DjangoGUIA.py
+++ DjangoGUIA.py
@@ -959,6 +959,22 @@ def index(request):
     user = request.user
     return render_to_response('index.html', {'usuario':user},context_instance=RequestContext(request))
 
+# En urls.py
+
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+from app import views
+from app.views import *
+import app.views as views
+from django.contrib.auth.decorators import login_required
+
+urlpatterns = [
+    url(r'^$', views.Login.as_view(), name='login'),
+    url(r'^register/', views.Register.as_view(), name='register'),
+    url(r'success/', login_required(views.Success.as_view(), login_url='login'), name='success'),
+    url(r'logout/$', views.Logout.as_view(), name='logout'),
+]
+
 ################################
 ##### if/else en templates #####
 ################################
