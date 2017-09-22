4a5d083802199d91be56c44c82df2fe8e0366e3f
aosorio <argenisosorio580@hotmail.com>
2017-09-22 14:43:20 -0400

    Añadidos tips a las guías

--- DjangoGUIA.py
+++ DjangoGUIA.py
@@ -2217,6 +2217,8 @@ ignorados.
 
 # pip install db-psycopg2
 
+# pip install psycopg2
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
