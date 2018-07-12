0422d00ac3ef3cae382f25639af5e0a7a2fb8bc3
aosorio <argenisosorio580@hotmail.com>
2018-07-12 12:06:36 -0400

    Agregados ejemplos de como colorear los prints en bash y convertir en base64 una imagen que venga de una url

--- BashScript.sh
+++ BashScript.sh
@@ -493,3 +493,35 @@ unzip /home/aosorio/tmp/x.zip
 
 # Imprimir por pantalla que finalizó la descarga
 echo -e '\n¡Descarga finalizada, el fichero fue descomprimido!\n'
+
+-----
+
+#!/bin/bash
+# Ejemplo de como colorear los prints en bash.
+# List colors
+# No Color | 0m
+# Black | 0;30
+# Dark Gray | 1;30
+# Red | 0;31
+# Light Red | 1;31
+# Green |0;32
+# Light Green | 1;32
+# Brown/Orange | 0;33
+# Yellow | 1;33
+# Blue | 0;34
+# Light Blue | 1;34
+# Purple | 0;35
+# Light Purple | 1;35
+# Cyan | 0;36
+# Light Cyan | 1;36
+# Light Gray | 0;37
+# White | 1;37
+NOCOLOR='\033[0m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+echo 'my text solo'
+printf "${YELLOW}xxxx \n"
+printf "${BLUE}yyyy \n"
+printf "${RED}zzzz \n${NOCOLOR}"
+printf "my text solo \n"
