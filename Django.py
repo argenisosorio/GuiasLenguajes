6c896b55ab84966fc3e788914e833838e51db847
aosorio <argenisosorio580@hotmail.com>
2018-09-19 15:13:47 -0400

    Agregado nuevo ejemplo a la guía de Django

--- Django.py
+++ Django.py
@@ -4400,3 +4400,27 @@ def change_password(request):
     No hay objetos registrados
   {% endif %}
 {% endblock %}
+
+###############################################################
+##### Realizando calculos con los valores en el models.py #####
+###############################################################
+
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.core.urlresolvers import reverse
+
+
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=200)
+    actividad_1 = models.IntegerField()
+    actividad_2 = models.IntegerField()
+
+    def _get_total(self):
+        return self.actividad_1+self.actividad_2
+    total = property(_get_total)
+
+    def __unicode__(self):
+        return self.nombre
+
+    def get_absolute_url(self):
+        return reverse('registro:editar', kwargs={'pk': self.pk})
